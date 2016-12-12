a1eec29df59b3ff8fc81786e406fdbf7ab1c7e06
Lee-KangChen <Lee-KangChen@CLK-Air.local>
2016-12-12 19:46:25 +0800

    Pins CRUD new and form

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,4 +1,24 @@
 class PinsController < ApplicationController
   def index
   end
+
+  def new
+    @pin = Pin.new
+  end
+
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: "Pin was successfully created"
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def pin_params
+    params.require(:pin).permit(:title, :description)
+  end
 end
